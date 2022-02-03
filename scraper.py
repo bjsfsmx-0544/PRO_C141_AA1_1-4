ec965dc3217967be8db5c00a13c21e9dd09f82d9
procodingclass <88050205+procodingclass@users.noreply.github.com>
2022-02-04 00:34:55 +0530

    Add files via upload

---
+++ scraper.py
@@ -0,0 +1,19 @@
+from selenium import webdriver
+from bs4 import BeautifulSoup
+import time
+import csv
+START_URL = "https://exoplanets.nasa.gov/exoplanet-catalog/"
+browser = webdriver.Edge("./msedgedriver.exe")
+browser.get(START_URL)
+time.sleep(10)
+
+#Define Scrape function
+
+    #Create lists for data
+    
+    # Use for loop for scraping through all the pages
+    
+#Write the data into csv file
+    
+
+
