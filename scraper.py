61b1b8b9e7fbe5d6dac67030750f5861a98d8878
bjsfsmx-0544 <94998868+bjsfsmx-0544@users.noreply.github.com>
2022-07-01 09:34:52 -0500

    Traducción 1.00

--- scraper.py
+++ scraper.py
@@ -4,10 +4,10 @@ from bs4 import BeautifulSoup
 import time
 import pandas as pd
 
-# NASA Exoplanet URL
+# Enlace a NASA Exoplanet
 START_URL = "https://exoplanets.nasa.gov/exoplanet-catalog/"
 
-# Webdriver
+# Controlador web
 browser = webdriver.Chrome("D:/Setup/chromedriver_win32/chromedriver.exe")
 browser.get(START_URL)
 
@@ -15,29 +15,25 @@ time.sleep(10)
 
 planets_data = []
 
-# Define Exoplanet Data Scrapping Method
+# Definir el método de extracción de datos para Exoplanet
 def scrape():
 
     for i in range(0,10):
         print(f'Scrapping page {i+1} ...' )
 
-        ## ADD CODE HERE ##
+        ## AGREGAR EL CÓDIGO AQUÍ ##
         
 
 
 
         
-# Calling Method    
+# Llamada del método
 scrape()
 
-# Define Header
+# Definir los encabezados
 headers = ["name", "light_years_from_earth", "planet_mass", "stellar_magnitude", "discovery_date"]
 
-# Define pandas DataFrame   
+# Definir el dataframe de Pandas
 
 
-# Convert to CSV
-
-    
-
-
+# Convertir a CSV
